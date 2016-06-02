42bf480d55f1a1a22e6f04b0904808eafe0fdc8c
Wonnam Jang <wn.jang@samsung.com>
2016-06-02 21:33:07 +0900

    Fix bug for get private data
    Change-Id: Ie64e4f1dda5f8b04702e497b238591e260e3aca1
    Signed-off-by: Wonnam Jang <wn.jang@samsung.com>

--- client/tts_dbus.c
+++ client/tts_dbus.c
@@ -658,10 +658,8 @@ int tts_dbus_request_get_private_data(int uid, const char* key, char** data)
 
 		if (0 == result) {
 			SLOG(LOG_DEBUG, TAG_TTSC, "<<<< tts get private data : result(%d)", result);
-			if (NULL != temp) {
+			if (NULL != data && NULL != temp) {
 				*data = strdup(temp);
-				free(temp);
-				temp = NULL;
 			}
 		} else {
 			SLOG(LOG_ERROR, TAG_TTSC, "<<<< tts get private data : result(%d)", result);
